e5fee51e90ff7fa9e358f7a506fb268777af7191
nauchikus <nauchikus@yandex.ru>
2018-12-21 21:46:53 +0300

    fix color output in build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 export WORKING_DIR=`pwd`
 
-echo -en "\033[46m [book build] start"
+echo "\033[46m [book build] start"
 
 #build book
 npm run build
@@ -10,13 +10,14 @@ npm run build
 #copy app
 git clone --depth=1 --branch=gh-pages   https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git app
 
+#remove prev book files
 if [ -d $WORKING_DIR/app/book ]; then
-rm -rv $WORKING_DIR/app/book
+rm -r $WORKING_DIR/app/book
 echo "\033[46m > delete dir book from app"
 fi
 
-
+#copy book, images and app
 cp -rf $WORKING_DIR/app/. $WORKING_DIR/dest
 cp -rf $WORKING_DIR/images $WORKING_DIR/dest/book
 
-echo -en "\033[46m [book build] complete"
+echo "\033[46m [book build] complete"
