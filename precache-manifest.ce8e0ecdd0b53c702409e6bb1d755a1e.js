bd91271811052651ea7c3c3d660f3d4ad4492ac4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-22 16:28:39 +0000

    Deploy nauchikus/typescript-definitive-guide to github.com/nauchikus/typescript-definitive-guide.git:gh-pages

---
+++ precache-manifest.ce8e0ecdd0b53c702409e6bb1d755a1e.js
@@ -0,0 +1,109 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c57d5476edbe210899a",
+    "url": "/typescript-definitive-guide/css/app.726b422a.css"
+  },
+  {
+    "revision": "5c57d5476edbe210899a",
+    "url": "/typescript-definitive-guide/js/app.9ec99a84.js"
+  },
+  {
+    "revision": "7b9005dd817da874e646",
+    "url": "/typescript-definitive-guide/css/chapter.8cdea95e.css"
+  },
+  {
+    "revision": "7b9005dd817da874e646",
+    "url": "/typescript-definitive-guide/js/chapter.0423ae33.js"
+  },
+  {
+    "revision": "3d384f9e77c66e2c3f9c",
+    "url": "/typescript-definitive-guide/css/chunk-vendors.c4ea8f33.css"
+  },
+  {
+    "revision": "3d384f9e77c66e2c3f9c",
+    "url": "/typescript-definitive-guide/js/chunk-vendors.4a5ad94a.js"
+  },
+  {
+    "revision": "b71458b4979c02a24fe7",
+    "url": "/typescript-definitive-guide/css/contents.34684a4a.css"
+  },
+  {
+    "revision": "b71458b4979c02a24fe7",
+    "url": "/typescript-definitive-guide/js/contents.28066e30.js"
+  },
+  {
+    "revision": "999b531810437ee7eccd",
+    "url": "/typescript-definitive-guide/css/hode.46fffa41.css"
+  },
+  {
+    "revision": "999b531810437ee7eccd",
+    "url": "/typescript-definitive-guide/js/hode.f424a4d6.js"
+  },
+  {
+    "revision": "16f1846e4a44b2e41b65",
+    "url": "/typescript-definitive-guide/css/not-found.91f525e7.css"
+  },
+  {
+    "revision": "16f1846e4a44b2e41b65",
+    "url": "/typescript-definitive-guide/js/not-found.7c2b069f.js"
+  },
+  {
+    "url": "/typescript-definitive-guide/img/icons.16e33dde.svg"
+  },
+  {
+    "revision": "94652ce68fe98ed1882e7b11b3639d51",
+    "url": "/typescript-definitive-guide/index.html"
+  },
+  {
+    "revision": "9d63b0e33b5f8482505f6b76bbb3c98f",
+    "url": "/typescript-definitive-guide/404.html"
+  },
+  {
+    "revision": "5450519d774414391cd5b7a74b93211e",
+    "url": "/typescript-definitive-guide/assets/icons/png/app-error__icon.png"
+  },
+  {
+    "revision": "585bfd8636d295630b7b0a0ec765e92d",
+    "url": "/typescript-definitive-guide/assets/icons/png/social/social__icon__192x192.png"
+  },
+  {
+    "revision": "61a1540df3f8c547d652d4fcc596080e",
+    "url": "/typescript-definitive-guide/assets/icons/png/social/social__icon__32x32.png"
+  },
+  {
+    "revision": "3d9042a49503c49aadeb98014f7ceb29",
+    "url": "/typescript-definitive-guide/assets/icons/png/spelling_error__icon.png"
+  },
+  {
+    "revision": "cd5558072231663c3aa77a9597fad1f2",
+    "url": "/typescript-definitive-guide/assets/icons/png/telegram__icon.png"
+  },
+  {
+    "revision": "6109cbfaf077ebc14231b4c0478cc6cf",
+    "url": "/typescript-definitive-guide/assets/icons/png/yandex_mail__icon.png"
+  },
+  {
+    "revision": "2e7638146bc58606e191058dc55eb869",
+    "url": "/typescript-definitive-guide/assets/icons/svg/baseline-link-24px.svg"
+  },
+  {
+    "revision": "d7b99dae43aa9a236ebd00feb1b258e6",
+    "url": "/typescript-definitive-guide/assets/images/app-logo.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/typescript-definitive-guide/robots.txt"
+  },
+  {
+    "revision": "3b94d61243da09afbd7904ff16c84bf3",
+    "url": "/typescript-definitive-guide/assets/icons/png/social/social__icon__512x512.png"
+  },
+  {
+    "revision": "c3a1f26ab83d7f43ffa594e8d2358669",
+    "url": "/typescript-definitive-guide/assets/icons/png/social/social__icon__1200x630.png"
+  },
+  {
+    "revision": "6ff95256bf356aa51f9ddd917fe89a57",
+    "url": "/typescript-definitive-guide/assets/images/book-cover-with-ufo.png"
+  }
+];
