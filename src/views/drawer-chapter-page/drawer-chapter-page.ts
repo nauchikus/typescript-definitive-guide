3488956f455eb7ba331336c554872e27281f32bc
nauchikus <nauchikus@yandex.ru>
2018-12-24 21:14:25 +0300

    fix(components): fix subchapter path by route /book/contents/*
    change subchapter path by route /book/contents/* from relative to absolute path
    
    МЕТА ДАННЫЕ: close#4

--- src/views/drawer-chapter-page/drawer-chapter-page.ts
+++ src/views/drawer-chapter-page/drawer-chapter-page.ts
@@ -1,7 +1,12 @@
 import { mapGetters } from 'vuex';
+import Vue, { ComponentOptions } from "vue";
 
-export default {
+export interface IComponent extends Vue {}
+
+const component: ComponentOptions<IComponent> = {
     computed: {
         ...mapGetters(['bookCurrentSubchapterAll'])
-    }
+    },
 };
+
+export default component;
