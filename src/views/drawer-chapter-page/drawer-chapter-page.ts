f41e9293dad78e0a510b9e400937bbf284800c7d
nauchikus <nauchikus@yandex.ru>
2019-01-09 20:22:28 +0300

    refactor(product): change yandex metrika
    change yandex metrika

--- src/views/drawer-chapter-page/drawer-chapter-page.ts
+++ src/views/drawer-chapter-page/drawer-chapter-page.ts
@@ -1,12 +1,12 @@
 import { mapGetters } from 'vuex';
-import Vue, { ComponentOptions } from "vue";
+import Vue, { ComponentOptions } from 'vue';
 
 export interface IComponent extends Vue {}
 
 const component: ComponentOptions<IComponent> = {
     computed: {
         ...mapGetters(['bookCurrentSubchapterAll'])
-    },
+    }
 };
 
 export default component;
