30f7f018d6b08bbe0fa273cc4df75e36eda9ff49
nauchikus <nauchikus@yandex.ru>
2019-01-31 21:53:50 +0300

    change build.sh

--- build.sh
+++ build.sh
@@ -13,13 +13,13 @@ cross-env NODE_ENV=production npm run build
 git clone --depth=1 --branch=gh-pages   https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git app
 
 #remove prev book files
-if [ -d $WORKING_DIR/app/book ]; then
-rm -r $WORKING_DIR/app/book
-echo "$CYAN_COLOR > delete dir book from app"
+if [ -d $WORKING_DIR/app/whats-is-new ]; then
+rm -r $WORKING_DIR/app/whats-is-new
+echo "$CYAN_COLOR > delete dir whats-is-new from app"
 fi
 
 #copy book, images and app
 cp -rf $WORKING_DIR/app/. $WORKING_DIR/dest
-cp -rf $WORKING_DIR/images $WORKING_DIR/dest/book
+#cp -rf $WORKING_DIR/images $WORKING_DIR/dest/whats-is-new
 
 echo "$CYAN_COLOR [book build] complete"
