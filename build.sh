8679d7aed0520864e51669f54e19eb2a85104a59
nauchikus <nauchikus@yandex.ru>
2019-02-01 12:47:11 +0300

    change build.sh

--- build.sh
+++ build.sh
@@ -8,27 +8,22 @@ export CYAN_COLOR=\033[46m
 echo " > [meta WorkingDir] $WORKING_DIR"
 echo " [build WhatsIsNew] Start Build."
 
-#remove hashed directory /dest
-if [ -d $WORKING_DIR/dest ]; then
-rm -r $WORKING_DIR/dest
-echo "$CYAN_COLOR > delete dir /dest"
-fi
 
-#build book
+#build notes
 cross-env NODE_ENV=production npm run build
 
-#copy app
+#clone app
 git clone --depth=1 --branch=gh-pages   https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git app
 
 
 
-#remove prev book files
+#remove prev notes files from gh-pages
 if [ -d $WORKING_DIR/app/whats-is-new ]; then
 rm -r $WORKING_DIR/app/whats-is-new
 echo "$CYAN_COLOR > delete dir whats-is-new from app"
 fi
 
-#copy book, images and app
+#copy application from /app to /dest
 cp -rf $WORKING_DIR/app/. $WORKING_DIR/dest
 #cp -rf $WORKING_DIR/images $WORKING_DIR/dest/whats-is-new
 
