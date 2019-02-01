7053be43088faf5f2867f3c2c398e20aabee9b57
nauchikus <nauchikus@yandex.ru>
2019-02-01 11:50:19 +0300

    add script delete prev dir /dest in build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,19 @@ export CYAN_COLOR=\033[46m
 echo " > [meta WorkingDir] $WORKING_DIR"
 echo " [build WhatsIsNew] Start Build."
 
+#remove hashed directory /dest
+if [ -d $WORKING_DIR/dest ]; then
+rm -r $WORKING_DIR/dest
+echo "$CYAN_COLOR > delete dir /dest"
+fi
+
 #build book
 cross-env NODE_ENV=production npm run build
 
 #copy app
 git clone --depth=1 --branch=gh-pages   https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git app
+
+
 
 #remove prev book files
 if [ -d $WORKING_DIR/app/whats-is-new ]; then
