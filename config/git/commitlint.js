fd74b79a4d1baf92e71ca127b4cc5f34e40cafb4
nauchikus <nauchikus.mail@gmail.com>
2020-04-07 10:13:31 +0300

    commit

---
+++ config/git/commitlint.js
@@ -0,0 +1,47 @@
+// Файл создан на основе @commitlint/config-conventional
+
+module.exports = {
+  rules: {
+    // Тело коммита должно начинаться с пустой строки
+    "body-leading-blank": [2, "always"],
+
+    // Нижний колонтитул коммита должен начинаться с пустой строки
+    "footer-leading-blank": [2, "always"],
+
+    // Максимальная длина заголовка 72 символа
+    "header-max-length": [2, "always", 72],
+
+    // Область всегда только в нижнем регистре
+    "scope-case": [2, "always", "lower-case"],
+
+    // Описание не может быть пустым
+    "subject-empty": [2, "never"],
+
+    // Описание не должно заканчиваться '.'
+    "subject-full-stop": [2, "never", "."],
+
+    // Тип всегда только в нижнем регистре
+    "type-case": [2, "always", "lower-case"],
+
+    // Тип не может быть пустым
+    "type-empty": [2, "never"],
+
+    // Перечислим все возможные варианты коммитов
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test"
+      ]
+    ]
+  }
+};
