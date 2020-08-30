3185d199c101ea379b89e27dcd83a2849e531408
nauchikus <nauchikus@yandex.ru>
2020-08-30 19:30:20 +0400

    fix generate images dir config.

--- config/old-to-new-book/generate-images-dir.js
+++ config/old-to-new-book/generate-images-dir.js
@@ -5,10 +5,10 @@ const CHAPTERS_DIR = path.relative(`./`, `../../book/ru/chapters`);
 
 
 const generateImageDir = async ({CHAPTERS_DIR}) => {
-    let chapterDirAll = await Promise.all(Array.from(fs.readdir(CHAPTERS_DIR)));
+    let chapterDirAll = await fs.readdir(CHAPTERS_DIR);
 
-    return await Promise.all(chapterDirAll.map(async chapterDir => {
-        return fs.mkdir(path.join(CHAPTERS_DIR, `images`));
+    return await Promise.all(chapterDirAll.map( chapterDir => {
+        return fs.mkdir(path.join(CHAPTERS_DIR, chapterDir, `images`));
     }));
 }
 
