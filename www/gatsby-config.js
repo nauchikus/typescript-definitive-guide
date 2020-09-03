de8be8ef35e4917ab81a72a4da697d1e44079758
nauchikus <nauchikus@yandex.ru>
2020-09-03 13:23:31 +0400

    ci(прочая конфигурация): add favicon
    add favicon from old app

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -142,7 +142,7 @@ const getPlugins = ({ locale, lang }) => [
       background_color: '#663399',
       theme_color: '#663399',
       display: 'minimal-ui',
-      icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      icon: 'assets/images/app-logos/app-logo.svg' // This path is relative to the root of the site.
     }
   },
   {
