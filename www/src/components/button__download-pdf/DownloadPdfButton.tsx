71e986bddebba579004641d3d8bdaa1c724deda2
nauchikus <nauchikus@yandex.ru>
2020-09-14 20:22:23 +0400

    www(доработка имеющегося функционала): add ya-metrica to button
    downtload-pdf-button

--- www/src/components/button__download-pdf/DownloadPdfButton.tsx
+++ www/src/components/button__download-pdf/DownloadPdfButton.tsx
@@ -13,11 +13,14 @@ export const DownloadPdfButton:FC<IDownloadPdfButtonProps>=({})=>{
     PdfSharedLocalization,
   ]>( LocalizationPaths.PdfShared );;
 
-
+  // @ts-ignore
+  const onClick = () => ym(51846272, "reachGoal", "download-pdf-button");
 
   return(
-    <a id="download-pdf-button" className="download-pdf-button" href={pdfShared.downloadPdfBookButton.href}
-       download={true}>
+    <a id="download-pdf-button"
+       className="download-pdf-button"
+       href={pdfShared.downloadPdfBookButton.href}
+       download={true} onClick={onClick}>
       <PdfSvgIcon className="download-pdf-button__svg-icon"/>
       <span className="download-pdf-button__label">{pdfShared.downloadPdfBookButton.title}</span>
 
