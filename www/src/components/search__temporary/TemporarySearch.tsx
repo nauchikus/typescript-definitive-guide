aeaf5c6eeaf45d0e0ebcce4f5bd1aff7af714532
nauchikus <nauchikus@yandex.ru>
2020-09-15 10:44:00 +0400

    www(доработка имеющегося функционала): change search input id
    from search to text

--- www/src/components/search__temporary/TemporarySearch.tsx
+++ www/src/components/search__temporary/TemporarySearch.tsx
@@ -3,15 +3,13 @@ import React, { FC, FormEvent, useRef } from "react";
 interface ISearchProps {
 }
 
-const SCALE_CONTROL_ID = "app-search";
 
 export const TemporarySearch: FC<ISearchProps> = ({} ) => {
   return (
-    <form className="search">
+    <form id="search-form" className="search">
       <input className="search__input"
-             type="search"
+             type="text"
              id="algolia-search"
-             name="app-search-query"
              placeholder="Поиск..."/>
     </form>
   );
