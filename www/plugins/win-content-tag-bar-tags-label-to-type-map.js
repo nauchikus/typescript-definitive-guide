be285d996345ad32a58b6751a08bb84e3b305d43
nauchikus <nauchikus@yandex.ru>
2020-09-20 15:17:56 +0400

    www(изменение стилей): add styles
    add styles for win tags

--- www/plugins/win-content-tag-bar-tags-label-to-type-map.js
+++ www/plugins/win-content-tag-bar-tags-label-to-type-map.js
@@ -3,6 +3,8 @@ class WinContentTagBarTagsLabelToTypeMap{
     static DEFAULT_TAG_INFO_CLASSES=[`content__tag-bar-tag_default`];
     static labelToClassesMap = new Map( [
         [`Критическое изменение`, { classes: [`content__tag-bar-tag_critical`], priority: 99 }],
+        [`Нововведение`, { classes: [`content__tag-bar-tag_new`], priority: 1 }],
+        [`Изменение`, { classes: [`content__tag-bar-tag_change`], priority: 1 }],
         [`Компилятор`, { classes: [`content__tag-bar-tag_compiler`], priority: 1 }],
         [`*`, { classes: [`content__tag-bar-tag_default`], priority: 0 }]
     ] );
