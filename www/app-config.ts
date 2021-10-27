91c6ced89f2e23a0fffa8a3432cb6ccf817f15a6
nauchikus <nauchikus@yandex.ru>
2021-10-27 08:25:25 +0300

    www(исправление бага): add link to telegram and github btn.
    add link to telegram and github btn.
    
    МЕТА ДАННЫЕ: Closes #167

--- www/app-config.ts
+++ www/app-config.ts
@@ -1,4 +1,6 @@
 export const appConfig = {
   title: `TDG-подробное руководство по языку TypeScript`,
-  description: `Подробная документация и книга в одном по языку TypeScript.`
+  description: `Подробная документация и книга в одном по языку TypeScript.`,
+  github: `https://github.com/nauchikus/typescript-definitive-guide`,
+  telegram: `https://t.me/nauchikus`,
 }
--- www/components/app-link-bar/AppLinkNav.tsx
+++ www/components/app-link-bar/AppLinkNav.tsx
@@ -1,18 +1,19 @@
 import React, {FC, ReactElement, Reducer, useReducer, useState} from "react";
 import {GithubIcon, MoreHorizontalIcon, MoreVerticalIcon, TelegramIcon} from "../icons/icons";
 import {SvgLinkButton} from "../buttons/svg-link-button/SvgLinkButton";
+import { appConfig } from "../../app-config";
 
 
 export const AppLinkBar: FC = () => (
     <nav className="app-link-bar">
         <ul className="menu app-link-bar__menu">
             <li>
-                <SvgLinkButton className="app-link-bar__icon-btn" href="" title="Следить за выходом новых версий в Telegram">
+                <SvgLinkButton className="app-link-bar__icon-btn" href={appConfig.telegram} title="Следить за выходом новых версий в Telegram">
                     <TelegramIcon />
                 </SvgLinkButton>
             </li>
             <li>
-                <SvgLinkButton className="app-link-bar__icon-btn" href="" title="Посетить Github">
+                <SvgLinkButton className="app-link-bar__icon-btn" href={appConfig.github} title="Посетить Github">
                     <GithubIcon className="app-link-bar__github-svg-icon" />
                 </SvgLinkButton>
             </li>
