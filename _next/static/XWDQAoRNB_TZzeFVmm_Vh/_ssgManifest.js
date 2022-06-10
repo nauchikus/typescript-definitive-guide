65d98cbe321633f364bf517bf9644e0fe4d37022
nauchikus <nauchikus@users.noreply.github.com>
2022-06-10 09:21:36 +0000

    deploy: 68874331b0833c04835a60c6ab259849f835787a

--- _next/static/XWDQAoRNB_TZzeFVmm_Vh/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fbook\u002Fchapters","\u002Fwhat-is-new","\u002F","\u002Fbook\u002Fchapters\u002F[chapterId]","\u002Fwhat-is-new\u002F[version]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
