304cb5e06f4b41390237c2c234d13d766e76f1c2
nauchikus <nauchikus@yandex.ru>
2022-06-10 12:27:15 +0400

    www(доработка имеющегося функционала): change ya metrics
    change ya metrics

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -11,23 +11,23 @@ function MyApp({ Component, pageProps }: AppProps) {
     useDocSearch();
 
 
-
-  return (
-    <BaseAppLayout>
-      <Head>
-        <MetaMultiDescription title={appConfig.title} description={appConfig.description} />
-        <meta name="viewport"
-              content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no"/>
-        <link key="algolia_css"
-              rel="stylesheet"
-              href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"/>
-        <script key="algolia_script"
-                type="text/javascript"
-                src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>
-        <title>{appConfig.title}</title>
-      </Head>
-      <Component { ...pageProps } />
-    </BaseAppLayout>
-  );
+    return (
+        <BaseAppLayout>
+            <Head>
+                <MetaMultiDescription title={ appConfig.title } description={ appConfig.description }/>
+                <meta name="yandex-verification" content="d7555ce264b8a099"/>
+                <meta name="viewport"
+                      content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+                <link key="algolia_css"
+                      rel="stylesheet"
+                      href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"/>
+                <script key="algolia_script"
+                        type="text/javascript"
+                        src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script>
+                <title>{ appConfig.title }</title>
+            </Head>
+            <Component { ...pageProps } />
+        </BaseAppLayout>
+    );
 }
 export default MyApp
