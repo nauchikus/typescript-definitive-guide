f33ce796be83557259611848250176170755dc40
nauchikus <nauchikus@yandex.ru>
2022-09-13 12:03:30 +0400

    www(доработка имеющегося функционала): change ga
    Change ga

--- www/analytics/google-analytics.tsx
+++ www/analytics/google-analytics.tsx
@@ -1,19 +1,19 @@
 import { FC, useEffect } from "react";
+import Script from "next/script";
 
 export const GoogleAnalytics:FC = () => {
-    useEffect( () => {
-        window[`dataLayer`] = window[ `dataLayer` ] ?? [];
-
-        function gtag ( ...rest ) {
-            window[ `dataLayer` ].push( arguments );
-        }
-
-        gtag( 'js', new Date() );
-        gtag( 'config', 'G-K2BJ26CT7F' );
-    }, [] );
     return (
         <>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-K2BJ26CT7F"></script>
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-K2BJ26CT7F" strategy="afterInteractive" />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){window.dataLayer.push(arguments);}
+                  gtag('js', new Date());
+        
+                  gtag('config', 'G-K2BJ26CT7F');
+                `}
+            </Script>
         </>
     )
 }
